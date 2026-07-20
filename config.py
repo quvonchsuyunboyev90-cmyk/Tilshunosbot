e282b92cae93f0a240cef874a33e3b7fac675fc7
quvonchsuyunboyev90-cmyk <quvonchsuyunboyev90@gmail.com>
2026-07-20 14:04:31 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+RAPIDAPI_HOST = "free-api-live-football-data.p.rapidapi.com"
