b576101c34f869b9db473c85628391a5f88d6b97
quvonchsuyunboyev90-cmyk <quvonchsuyunboyev90@gmail.com>
2026-07-20 14:38:23 +0500

    Update football_api.py

--- football_api.py
+++ football_api.py
@@ -3,3 +3,11 @@
 from services import *
 
 print("Football API yuklandi")
+def get_team_details(team_id):
+    """
+    Jamoa haqida batafsil ma'lumot qaytaradi.
+    """
+    return call_sport_api(
+        "/football-team-detail",
+        {"teamId": team_id}
+    )
